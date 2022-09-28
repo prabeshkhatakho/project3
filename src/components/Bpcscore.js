1866250fe61fb2e7967bf4f0ca747536f8076303
prabesh <prabeshkhatakho77@gmail.com>
2022-09-28 13:20:15 +0545

    asd

--- src/components/Bpcscore.js
+++ src/components/Bpcscore.js
@@ -5,24 +5,25 @@ import './bpcscore.css'
 
 const Bpcscore=()=> {
 
-    const [state, setState] = useState('neutral');
+    const [state, setState] = useState('');
 
     return (   
     <div className="bpcscore">
-        <h1>personality check question</h1>
+        <h1>Personality check question.</h1>
         {
             Data.map( data => {
                 return (
                     <body>
                     <div className="box">
-                            <h2>{data.id}. {data.question}</h2>
+                            <h1>{data.id}. {data.question}</h1>
                             <form>
                             <input className="first_input" type='radio'  id='react-radio-button-group-1' name='number' value='strong agree' />
-                            <label className="first_label" for="react-radio-button-group-1"></label>
+                            <label for="react-radio-button-group-1"></label>
                             <input className="secondbutton" type='radio'  id='react-radio-button-group-1' name='number' value='agree' />
                             <input className="thirdbutton" type='radio'  id='react-radio-button-group-1' name='number' value='neutral' />
                             <input className="fourthbutton" type='radio'  id='react-radio-button-group-1' name='number' value='strong disagree' />
                             <input className="fifthbutton" type='radio'  id='react-radio-button-group-1' name='number' value='disagree' />
+                            <div className="line"></div>
                             </form>              
             </div>
             </body>     
