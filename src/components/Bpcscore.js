0ae0f3bc31c192fdf7968da2888464cff9401c23
prabesh <prabeshkhatakho77@gmail.com>
2022-09-28 20:21:26 +0545

    commit

--- src/components/Bpcscore.js
+++ src/components/Bpcscore.js
@@ -17,12 +17,12 @@ const Bpcscore=()=> {
                     <div className="box">
                             <h1>{data.id}. {data.question}</h1>
                             <form>
-                            <input className="first_input" type='radio'  id='react-radio-button-group-1' name='number' value='strong agree' />
+                            <input className="first_radio_button" type='radio'  id='react-radio-button-group-1' name='number' value='strong agree' />
                             <label for="react-radio-button-group-1"></label>
-                            <input className="secondbutton" type='radio'  id='react-radio-button-group-1' name='number' value='agree' />
-                            <input className="thirdbutton" type='radio'  id='react-radio-button-group-1' name='number' value='neutral' />
-                            <input className="fourthbutton" type='radio'  id='react-radio-button-group-1' name='number' value='strong disagree' />
-                            <input className="fifthbutton" type='radio'  id='react-radio-button-group-1' name='number' value='disagree' />
+                            <input className="second_radio_button" type='radio'  id='react-radio-button-group-1' name='number' value='agree' />
+                            <input className="third_radio_button" type='radio'  id='react-radio-button-group-1' name='number' value='neutral' />
+                            <input className="second_radio_button" type='radio'  id='react-radio-button-group-1' name='number' value='strong disagree' />
+                            <input className="first_radio_button" type='radio'  id='react-radio-button-group-1' name='number' value='disagree' />
                             <div className="line"></div>
                             </form>              
             </div>
